7c6ca8a19438accd39a593f53fb435aaf9f945e4
vathasalya-4a <vathasalya.pakalapati@4a-consulting.com>
2024-11-11 13:46:53 -0500

    commit changes

--- middleware.ts
+++ middleware.ts
@@ -1,32 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
-
+ 
 export const config = {
   matcher: [
     "/((?!api/|_next/|_static/|_vercel|[\\w-]+\\.\\w+).*)",
   ],
 };
-
+ 
 export default async function middleware(req: NextRequest) {
   const url = req.nextUrl;
 
-  console.log("Request URL:", url);
-
-  let hostname = req.headers
-    .get("host")!
-    .replace(".localhost:3000", `.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
-  
-  console.log(`app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
-
+  let hostname = req.headers.get("host")!;
+ 
   const searchParams = req.nextUrl.searchParams.toString();
   const path = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}` : ""}`;
   const session = await getToken({ req });
-  
-  console.log("Session:", session);
-  console.log("Path:", path);
 
-  // Middleware logic for different environments
-  if (hostname == `app.${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`) {
+  console.log(url)
+  console.log(hostname)
+  
+  if(process.env.NEXT_PUBLIC_ROOT_DOMAIN) {
+    hostname = hostname.replace("localhost:3000", `${process.env.NEXT_PUBLIC_ROOT_DOMAIN}`);
+  }
+
+  console.log(hostname)
+  console.log(session)
+  console.log(path)
+
+  if (hostname === "localhost:3000" || hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN) {
     if (!session && path !== "/login") {
       return NextResponse.redirect(new URL("/login", req.url));
     }
@@ -36,17 +37,6 @@ export default async function middleware(req: NextRequest) {
     if (session && path === "/") {
       return NextResponse.rewrite(new URL("/", req.url));
     }
-    return NextResponse.rewrite(new URL(`/app${path === "/" ? "" : path}`, req.url));
   }
-
-  if (
-    hostname === "localhost:3000" ||
-    hostname === process.env.NEXT_PUBLIC_ROOT_DOMAIN
-  ) {
-    return NextResponse.rewrite(
-      new URL(`/home${path === "/" ? "" : path}`, req.url),
-    );
-  }
-  
   return NextResponse.rewrite(url);
 }
